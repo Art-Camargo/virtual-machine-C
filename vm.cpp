e443d4d43a642585b7920b1ddeea3c53815a4d39
artur <arturcamargo242@gmail.com>
2025-04-05 18:33:56 -0300

    Refactor virtual machine memory management and execution logic

--- vm.cpp
+++ vm.cpp
@@ -1,131 +1,105 @@
-// WE ARE CREATING A VIRTUAL MACHINE! 
-// OPERATIONS, OPCODES, AND QUANTITY OF OPERANDS
-// (add) a0 = a1 + a2 (00) (3 operands) 
-// sub a0 = a1 - a2 01 3 
-// mul a0 = a1 * a2 03 3 
-// div a0 = a1 / a2 03 3
-// mv ao = mem[op] 04 2
-// st mem[op] = a0 05 2
-// jmp jmp op 06 1 
-// jeq jeq a0 a1 op 07 3
-// jgt jgq a0 a1 op 08 3 
-// jlt jlq a0 a1 op 09 3
-// w Print [op] 10 1 
-// r Read [op] 11 1
-// stp Stop machine 12 0
-
-
 #include <iostream>
-#include <string>
+#include <unistd.h>
 
 using namespace std;
-#define MEMORY_SIZE  320
 
-typedef struct  {
-  int opcode;
-  int operands[3]; 
-  int operand_count;
+#define INSTRUCTION_MEMORY_SIZE 320
+#define DATA_MEMORY_SIZE 256
+
+typedef struct {
+    int opcode;
+    int operands[3];
+    int operand_count;
 } instruction;
 
-
-void reset_memory(instruction memory[]) {
-  for (int i = 0; i < MEMORY_SIZE; i++) {
-    memory[i].opcode = -1; // -1 indicates an empty instruction
-    for (int j = 0; j < 3; j++) {
-      memory[i].operands[j] = 0;
+void reset_instruction_memory(instruction memory[]) {
+    for (int i = 0; i < INSTRUCTION_MEMORY_SIZE; i++) {
+        memory[i].opcode = -1;
+        memory[i].operand_count = 0;
+        for (int j = 0; j < 3; j++) {
+            memory[i].operands[j] = 0;
+        }
     }
-    memory[i].operand_count = 0;
-  }
 }
 
-void put(instruction memory[], int pc, int opcode, int operand_count, int operands[]) {
-  memory[pc].opcode = opcode;
-  memory[pc].operand_count = operand_count;
-  for (int i = 0; i < operand_count; i++) {
-    memory[pc].operands[i] = operands[i];
-  }
-} 
+void put(instruction memory[], int position, int opcode, int operand_count, int operands[]) {
+    memory[position].opcode = opcode;
+    memory[position].operand_count = operand_count;
+    for (int i = 0; i < operand_count; i++) {
+        memory[position].operands[i] = operands[i];
+    }
+}
 
 int get_opcode(instruction memory[], int pc) {
-  return memory[pc].opcode;
+    return memory[pc].opcode;
 }
 
-
-void exec(instruction mem[]) {
-  int pc = 0, stopped = 0, opcode = -1;
+void exec(instruction mem[], int data_memory[]) {
+  int pc = 0, stopped = 0, opcode;
   int registers[4] = {0};
-  while(pc < MEMORY_SIZE && !stopped) {
-    opcode = get_opcode(mem, pc);
-    if (opcode == -1) {
-      pc++;
-      continue;
-    }; 
-    switch(opcode) {
-      case 00: 
-        registers[mem[pc].operands[0]] = registers[mem[pc].operands[1]] + registers[mem[pc].operands[2]];
-        break;
-      case 01: 
-        registers[mem[pc].operands[0]] = registers[mem[pc].operands[1]] - registers[mem[pc].operands[2]];
-        break;
-      case 02: 
-        registers[mem[pc].operands[0]] = registers[mem[pc].operands[1]] * registers[mem[pc].operands[2]];
-        break;
-      case 03: 
-        if (registers[mem[pc].operands[2]] == 0) registers[mem[pc].operands[0]] = 0; // Avoid division by zero
-        else registers[mem[pc].operands[0]] = registers[mem[pc].operands[1]] / registers[mem[pc].operands[2]];
-        break;
-      case 04: 
-        registers[mem[pc].operands[0]] = mem[mem[pc].operands[1]].operands[0];
-        break;
-      case 05: 
-        mem[mem[pc].operands[0]].operands[0] = registers[mem[pc].operands[1]];
-        break;
-      case 06: 
-        pc = mem[pc].operands[0];
-        break;
-      case 07: 
-        if (registers[mem[pc].operands[0]] == registers[mem[pc].operands[1]]) pc = mem[pc].operands[2];
-        break;
-      case 8: 
-        if (registers[mem[pc].operands[0]] > registers[mem[pc].operands[1]]) pc = mem[pc].operands[2];
-        break;
-      case 9: 
-        if (registers[mem[pc].operands[0]] < registers[mem[pc].operands[1]]) pc = mem[pc].operands[2];
-        break;
-      case 10: 
-        cout << "Output: " << registers[mem[pc].operands[0]] << endl;
-        break;
-      case 11: 
-        cout << "Input: ";
-        cin >> registers[mem[pc].operands[0]];
-        break;
-      case 12: 
-        stopped = 1;
-        break;
-      default:
-        break; 
-    }
-    pc++;
-  }
 
+  while (pc < INSTRUCTION_MEMORY_SIZE && !stopped) {
+      opcode = mem[pc].opcode;
+
+      if (opcode == -1) {
+          pc++;
+          continue;
+      }
+
+      int reg_dest = mem[pc].operands[0];
+      int reg_op1 = mem[pc].operands[1];
+      int a2 = mem[pc].operands[2];
+
+      switch (opcode) {
+          case 0: registers[reg_dest] = registers[reg_op1] + registers[reg_op2]; break; // add
+          case 1: registers[reg_dest] = registers[reg_op1] - registers[reg_op2]; break; // sub
+          case 2: registers[reg_dest] = registers[reg_op1] * registers[reg_op2]; break; // mul
+          case 3: registers[reg_dest] = (registers[reg_op2] != 0) ? registers[reg_op1] / registers[reg_op2] : 0; break; // div
+          case 4: registers[reg_dest] = data_memory[reg_op1]; break;             // mv (load)
+          case 5: data_memory[reg_dest] = registers[reg_op1]; break;            // st (store)
+          case 6: pc = reg_dest; continue;                                // jmp
+          case 7: if (registers[reg_dest] == registers[reg_op1]) { pc = reg_op2; continue; } break; // jeq
+          case 8: if (registers[reg_dest] >  registers[reg_op1]) { pc = reg_op2; continue; } break; // jgt
+          case 9: if (registers[reg_dest] <  registers[reg_op1]) { pc = reg_op2; continue; } break; // jlt
+          case 10: cout << "Output: " << registers[reg_dest] << endl; break; // w
+          case 11: cout << "Input: "; cin >> registers[reg_dest]; break;    // r
+          case 12: stopped = 1; break;                               // stp
+      }
+
+      printf("registers: %d %d %d %d\n", registers[0], registers[1], registers[2], registers[3]);
+      pc++;
+      sleep(1);
+  }
 }
 
 
 int main() {
-  instruction memory[MEMORY_SIZE];
-  reset_memory(memory);
-  int operands[3] = {0, 1, 2};
-  put(memory, 0, 0, 3, operands); // add a0 = a1 + a2
-  put(memory, 1, 1, 3, operands); // sub a0 = a1 - a2
-  put(memory, 2, 2, 3, operands); // mul a0 = a1 * a2
-  put(memory, 3, 3, 3, operands); // div a0 = a1 / a2
-  put(memory, 4, 4, 2, operands); // mv a0 = mem[op]
-  put(memory, 5, 5, 2, operands); // st mem[op] = a0
-  put(memory, 10, 10, 1, operands); // w Print [op]
-  put(memory, 11, 11, 1, operands); // r Read [op]
-  put(memory, 12, 12, 0, operands); // stp Stop machine
-  put(memory, 13, 0, 3, operands); // add a0 = a1 + a2
-  put(memory, 14, 12, 0, operands); // stp Stop machine
-  exec(memory);
-  return 0; 
+  instruction instruction_memory[INSTRUCTION_MEMORY_SIZE];
+  int data_memory[DATA_MEMORY_SIZE] = {0};
+  reset_instruction_memory(instruction_memory);
+  data_memory[100] = 20; 
+  data_memory[101] = 5;  
+  int op1[] = {1, 100};
+  put(instruction_memory, 0, 4, 2, op1);
+  int op2[] = {2, 101};
+  put(instruction_memory, 1, 4, 2, op2);
+  int op3[] = {0, 1, 2};
+  put(instruction_memory, 2, 0, 3, op3);
+  int op4[] = {3, 1, 2};
+  put(instruction_memory, 3, 1, 3, op4);
+  int op5[] = {1, 0, 2};
+  put(instruction_memory, 4, 2, 3, op5);
+  int op6[] = {2, 1, 3};
+  put(instruction_memory, 5, 3, 3, op6);
+  int op7[] = {0};
+  put(instruction_memory, 6, 10, 1, op7);
+  int op8[] = {1};
+  put(instruction_memory, 7, 10, 1, op8);
+  int op9[] = {2};
+  int op10[] = {3};
+  put(instruction_memory, 9, 10, 1, op10);
+  put(instruction_memory, 10, 12, 0, NULL);
+  exec(instruction_memory, data_memory);
+
+  return 0;
 }
